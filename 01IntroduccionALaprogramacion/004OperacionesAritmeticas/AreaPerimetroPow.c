8d53a01bf1209c09b7ba7762fd1d4bb0bdee5448
[MarioS] <157059512+ChilyWily@users.noreply.github.com>
2024-01-31 12:53:02 -0600

    Carpeta Nueva

---
+++ 01IntroduccionALaprogramacion/004OperacionesAritmeticas/AreaPerimetroPow.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int lado;
+    int resultadoArea;
+    int resultadoPerimetro;
+
+    printf("Teclee el lado del cuadrado\n");
+    scanf("%d",&lado);
+    resultadoArea = pow(lado, 2);
+
+    printf("El area es: %d\n", resultadoArea);
+
+    printf("Calcular el perimetro\n");
+
+    resultadoPerimetro = lado*4;
+
+    printf("El perimetro es: %d\n", resultadoPerimetro);
+    
+
+
+    }
